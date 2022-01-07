5396523034f1226e5031085ffaf4a24043879283
Diórgenes Eugênio <diorgeneseugenio@Diorgeness-MacBook-Pro.local>
2022-01-06 21:29:12 -0300

    chore: add tailwindcss

--- app/root.tsx
+++ app/root.tsx
@@ -4,13 +4,18 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
+  ScrollRestoration,
 } from "remix";
 import type { MetaFunction } from "remix";
+import styles from "./tailwind.css";
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+  return { title: "Useless Facts" };
 };
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
 
 export default function App() {
   return (
